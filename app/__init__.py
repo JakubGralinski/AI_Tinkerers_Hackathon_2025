3fac973982e4bf3f44ac9c4f4d8f4a4d590a122e
mlaskowski7 <mtlaskowski7@gmail.com>
2025-04-25 21:12:18 +0200

    fix: resolve import bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import Config
-from app.extensions import db, api, ma
+from app.extensions import db, ma
 from app.routes.user_routes import user_bp
 from dotenv import load_dotenv
 
@@ -13,7 +13,6 @@ def create_app():
     # init extensions
     db.init_app(app)
     ma.init_app(app)
-    api.init_app(app)
     
     # register routes
     app.register_blueprint(user_bp, url_prefix="/api")
