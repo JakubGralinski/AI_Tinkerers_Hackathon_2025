7fe7ca2dfb77af7354836d007799b58e3c1b32af
mlaskowski7 <mtlaskowski7@gmail.com>
2025-04-26 07:00:01 +0200

    feat: initial agent orchestration

--- app/services/plan_service.py
+++ app/services/plan_service.py
@@ -1,29 +1,55 @@
-from agents import Runner
-from agents import Runner
 from app.agents.plan.exercise_plan_agent import exercise_plan_agent
-from app.agents.plan.meal_plan_agent import meal_plan_agent
+from app.agents.plan.meal_plan_agent import NutritionPlannerAgent
 from app.agents.plan.response_schemas import MealPlanSchema
 from app.agents.plan.response_schemas import ExercisePlanSchema
-import asyncio
+from app.agents.plan.tool_wrapper import Tool
+from openai import OpenAI
+from agents import Agent, Runner
+import json
+from app.config import Config
 
 
+with open("./demo/mfp_daily_totals_and_menu.json", encoding="utf-8") as f:
+    mfp_data = json.load(f)
+with open("./demo/strava_activities.json", encoding="utf-8") as f:
+    strava_data = json.load(f)
 
 async def plan_week(user_prompt: str):
-    meal_task = Runner.run(
-        meal_plan_agent,
-        user_prompt,
+    nutrition_planner_agent = NutritionPlannerAgent(
+        client=OpenAI(api_key=Config.OPENAI_API_KEY),
+        user_profile_path="./demo/user_profile.json"
     )
-    exercise_task = Runner.run(
-        exercise_plan_agent,
-        user_prompt,
+    
+    def run_meal_planner(self, input_text):
+        nutrition_planner_agent.plan_meals(mfp_data, strava_data, user_prompt)
+    
+    tools = [
+        Tool(name="Meal Planner", description="Creates a weekly meal plan based on nutrition data and goals.", agent=nutrition_planner_agent, run_func=run_meal_planner),
+        Tool(name="Exercise Planner", description="Creates a weekly training plan based on Strava activities and fitness goals.", agent=exercise_plan_agent),
+    ]
+    
+    plan_manager_agent = Agent(
+        name="Plan Manager",
+        instructions=(
+            "You are a fitness and nutrition manager AI.\n"
+            "Based on the user's request, you decide whether to use the Meal Planner agent "
+            "or the Exercise Planner agent or both. "
+            "Return the correct agent's response or both of thems combined. Always use at least one of them. You can combine their responses, but do not return anything not from those tools."
+        ),
+        handoffs=tools,
+        model="gpt-4o"
     )
 
-    meal_result, exercise_result = await asyncio.gather(meal_task, exercise_task)
+    result = await Runner.run(plan_manager_agent, user_prompt)
+    
+    try:
+        return {
+            "plan": result.final_output.model_dump(),
+        }
+    except Exception:
+        return {
+            "plan": result.final_output,
+        }
+    
 
-    meal_output = meal_result.final_output_as(MealPlanSchema)
-    exercise_output = exercise_result.final_output_as(ExercisePlanSchema)
-
-    return {
-        "meal_plan": meal_output.model_dump(),
-        "exercise_plan": exercise_output.model_dump()
-    }
+    
